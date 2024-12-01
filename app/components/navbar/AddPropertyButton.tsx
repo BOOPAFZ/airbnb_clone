7fabff7ccded1904431671e9cfab029b48703bcd
BOOPAFZ <bakhsoussa@gmail.com>
2024-12-01 06:01:24 -0500

    added catogries static

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,6 @@
 const AddPropertyButton = () => {
     return (
-        <div className="cursor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200">buttong</div>
+        <div className="cursor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200">Airbnb your home</div>
     )
 }
 
