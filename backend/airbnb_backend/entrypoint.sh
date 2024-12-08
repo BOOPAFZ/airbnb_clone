0c92c20d378254ccaf6b49d3d3760acea6c6f7b4
BOOPAFZ <bakhsoussa@gmail.com>
2024-12-08 08:14:22 -0500

    all set with docker

---
+++ backend/airbnb_backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Checking if database is running..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+        echo "Waiting for the database..."
+        sleep 1
+    done
+
+    echo "Database is up and running!"
+fi
+
+# Run Django commands
+python manage.py migrate
+exec python manage.py runserver 0.0.0.0:8000
+
+
